f7ac38f22a1a828132ca09f490d9212fa6359906
João Victor Colussi <victorcolussi@outlook.com>
2025-09-21 10:53:57 -0300

    fix: argument bug in usermanager

--- classes/UserValidator.php
+++ classes/UserValidator.php
@@ -1,21 +1,30 @@
 <?php
 
-class UserValidator{
-    public function validateEmail($email){
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-            throw new InvalidArgumentException("Email Inválido");
+class UserValidator
+{
+    public function validateEmail(string $email, UserRepository $repository): void
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException("Email inválido.");
+        }
+        
+        if ($repository->findByEmail($email)) {
+            throw new InvalidArgumentException("Email já está em uso.");
         }
     }
 
-    public function validatePassword($password){
-        if (strlen($password) < 8){
-            throw new InvalidArgumentException("A senha deve conter no minimo 8 caracteres");
+    public function validatePassword(string $password): void
+    {
+        if (strlen($password) < 8) {
+            throw new InvalidArgumentException("A senha deve conter no mínimo 8 caracteres.");
         }
-        if (!preg_match("/[0-9]/", $password)){
-            throw new InvalidArgumentException("A desenha deve conter ao menos um número");
+        
+        if (!preg_match('/[0-9]/', $password)) {
+            throw new InvalidArgumentException("A senha deve conter ao menos um número.");
         }
-        if (!preg_match("/[A-Z]/", $password)){
-            throw new InvalidArgumentException("A senha deve conter ao menos uma letra maiúscula");
+        
+        if (!preg_match('/[A-Z]/', $password)) {
+            throw new InvalidArgumentException("A senha deve conter ao menos uma letra maiúscula.");
         }
     }
 }
